4021af3ebf3a6df31b8cae056a667f70ab93f12f
tataw-cl <127490325+tataw-cl@users.noreply.github.com>
2023-10-24 17:08:57 +0100

    actual code for the circular queue

---
+++ DSA.C
@@ -0,0 +1,9 @@
+/* #include <stdio.h> 
+int main(){ 
+char *materials[ ] = {  "iron",  "copper",  "gold"}; 
+printf("Please remember these materials :\n"); 
+int i ; 
+for (i = 0; i < 3; i++) {  
+     printf("%s\n", materials[ i ]);}   
+     return 0; 
+} */
